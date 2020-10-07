6b15c5f5dc61e333c3dbfec41fd09dd40cd5302f
Victor Irzak <victor.irzak@zomp.com>
2020-10-07 14:16:19 -0400

    WIP: Convert action buttons to ICommand

--- MvvmPattern/MvvmPattern/MainModel.cs
+++ MvvmPattern/MvvmPattern/MainModel.cs
@@ -1,12 +1,32 @@
-﻿using System.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System.ComponentModel;
+using System.Windows.Input;
 
 namespace MvvmPattern
 {
     public class MainModel : INotifyPropertyChanged
     {
+        public MainModel()
+        {
+            DecrementCommand = new RelayCommand<bool>(DecrementExecute);
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
 
+        public ICommand DecrementCommand;
         private int _age = 30;
+
+        private void DecrementExecute(bool decrement)
+        {
+            if (decrement && Age > 13)
+            {
+                Age -= 1;
+            }
+            if (!decrement && Age < 120)
+            {
+                Age += 1;
+            }
+        }
 
         public int Age
         {
